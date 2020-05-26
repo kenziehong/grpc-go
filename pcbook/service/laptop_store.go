5e5484a0abc74906f5f0eb957c3462be7f45c50a
kenziehong <dinhthianhhong.uel@gmail.com>
2020-05-27 02:01:41 +0700

    server-streaming grpc at 9:22, lecture 11

--- pcbook/service/laptop_store.go
+++ pcbook/service/laptop_store.go
@@ -18,6 +18,8 @@ type LaptopStore interface {
 	Save(laptop *pb.Laptop) error
 	// Find finds a laptop by ID
 	Find(id string) (*pb.Laptop, error)
+	// Search searches for laptops with filter, returns one by one via the found function
+	Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 // InMemoryLaptopStore stores laptop in memory
@@ -73,4 +75,70 @@ func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
 	}
 
 	return other, nil
+}
+
+// Search searches for laptops with filter, returns one by one via the found function
+func (store *InMemoryLaptopStore) Search(
+	filter *pb.Filter,
+	found func(laptop *pb.Laptop) error,
+) error {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.GetMaxPriceUsd() {
+		return false
+	}
+
+	if laptop.GetCpu().GetNumberCores() < filter.GetMinCpuCores() {
+		return false
+	}
+
+	if laptop.GetCpu().GetMinGhz() < filter.GetMinCpuGhz() {
+		return false
+	}
+
+	if toBit(laptop.GetRam()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+
+	return true
+}
+
+func toBit(memory *pb.Memory) uint64 {
+	value := memory.GetValue()
+
+	switch memory.GetUnit() {
+	case pb.Memory_BIT:
+		return value
+	case pb.Memory_BYTE:
+		return value << 3 // 8 = 2^3
+	case pb.Memory_KILOBYTE:
+		return value << 13 // 1024 * 8 = 2^10 * 2^3 = 2^13
+	case pb.Memory_MEGABYTE:
+		return value << 23
+	case pb.Memory_GIGABYTE:
+		return value << 33
+	case pb.Memory_TERABYTE:
+		return value << 43
+	default:
+		return 0
+	}
 }
